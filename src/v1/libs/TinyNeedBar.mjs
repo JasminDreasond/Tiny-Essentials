de896f0d39e833eda1c3cb22d1b71cbe97ff513a
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2025-08-27 10:55:04 -0300

    TinyNeedBar --> value names changed.

--- src/v1/libs/TinyNeedBar.mjs
+++ src/v1/libs/TinyNeedBar.mjs
@@ -2,7 +2,16 @@ class TinyNeedBar {
   /**
    * @type {Map<string, {amount: number, multiplier: number}>}
    */
-  factors = new Map();
+  #factors = new Map();
+
+  /** @type {number} */
+  #maxValue;
+
+  /** @type {number} */
+  #currentValue;
+
+  /** @type {number} */
+  #infiniteValue;
 
   /**
    * @param {number} maxValue
@@ -10,11 +19,11 @@ class TinyNeedBar {
    * @param {number} baseDecayMulti
    */
   constructor(maxValue = 100, baseDecay = 1, baseDecayMulti = 1) {
-    this.maxValue = maxValue;
+    this.#maxValue = maxValue;
     this.setFactor('main', baseDecay, baseDecayMulti);
 
-    this.currentValue = maxValue;
-    this.infiniteValue = maxValue;
+    this.#currentValue = maxValue;
+    this.#infiniteValue = maxValue;
   }
 
   /**
@@ -23,14 +32,14 @@ class TinyNeedBar {
    * @param {number} multiplier
    */
   setFactor(key, amount, multiplier = 1) {
-    this.factors.set(key, { amount, multiplier });
+    this.#factors.set(key, { amount, multiplier });
   }
 
   /**
    * @param {string} key
    */
   removeFactor(key) {
-    this.factors.delete(key);
+    this.#factors.delete(key);
   }
 
   /**
@@ -47,24 +56,24 @@ class TinyNeedBar {
   tick() {
     let removedTotal = 0;
 
-    for (let [_, factor] of this.factors.entries()) {
+    for (let [_, factor] of this.#factors.entries()) {
       removedTotal += factor.amount * factor.multiplier;
     }
 
-    const prevValue = this.infiniteValue;
-    this.infiniteValue -= removedTotal;
-    this.currentValue = Math.max(0, this.currentValue - removedTotal);
+    const prevValue = this.#infiniteValue;
+    this.#infiniteValue -= removedTotal;
+    this.#currentValue = Math.max(0, this.#currentValue - removedTotal);
 
-    const removedPercent = (removedTotal / this.maxValue) * 100;
-    const currentPercent = (this.currentValue / this.maxValue) * 100;
+    const removedPercent = (removedTotal / this.#maxValue) * 100;
+    const currentPercent = (this.#currentValue / this.#maxValue) * 100;
 
     return {
       prevValue,
       removedTotal,
       removedPercent,
       currentPercent,
-      remainingValue: this.currentValue,
-      infiniteRemaining: this.infiniteValue,
+      remainingValue: this.#currentValue,
+      infiniteRemaining: this.#infiniteValue,
     };
   }
 }
